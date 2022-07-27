8b5232d6ededf3760206f051bb6e94468567d175
IzanDuesoLopez <108723551+IzanDuesoLopez@users.noreply.github.com>
2022-07-27 22:44:25 +0200

    Update Ejercicio17 DML.sql

--- TA15- DML/Ejercicio17 DML.sql
+++ TA15- DML/Ejercicio17 DML.sql
@@ -11,7 +11,7 @@ create table Superior(
     codigoCentro int, 
     curso varchar(10),
     idCiclo int,
-    constraint FK_id_ciclo foreign key (idCiclo) references Ciclos(id)
+    constraint FK_id_ciclo_superior foreign key (idCiclo) references Ciclos(id)
 );
 
 create table Medio(
@@ -19,7 +19,7 @@ create table Medio(
     codigoCentro int,
     curso varchar(10),
     idCiclo int,
-    constraint FK_id_ciclo foreign key (idCiclo) references Ciclos(id)
+    constraint FK_id_ciclo_medio foreign key (idCiclo) references Ciclos(id)
 );
 
 create table Horarios(
@@ -50,4 +50,53 @@ create table Asignaturas(
     codigoEuropeo int,
     dni_profesor int,
     constraint fk_dni_profesor foreign key(dni_profesor) references Profesores(dni)
+);
+
+create table Aulas(
+	id int auto_increment primary key,
+    nombre varchar(25),
+    numero int,
+    metros int,
+    estaVacia boolean,
+    idAsignatura int,
+    constraint fk_id_asignatura foreign key(idAsignatura) references Asignaturas(id)
+);
+
+create table RequiereAsignaturas(
+	id int auto_increment primary key,
+    idAsignatura int,
+    constraint fk_id_asignatura_requerida foreign key(idAsignatura) references Asignaturas(id)
+);
+
+create table Matriculas(
+	id int auto_increment primary key,
+    precio float,
+    curso varchar(25)
+);
+
+create table Iniciaciones(
+	id int auto_increment primary key,
+    codigoCentro int,
+    curso varchar(25),
+    idCiclo int,
+    constraint fk_id_ciclo_iniciaciones foreign key(idCiclo) references Ciclos(id)
+);
+
+create table Imparten(
+	idAsignatura int,
+    dni_profesor varchar(25),
+    idHora int,
+    idCiclo int,
+    foreign key(idAsignatura) references Asignaturas(id),
+    foreign key(dni_profesor) references Profesores(dni),
+    foreign key(idHora) references Horarios(id),
+    foreign key(idCiclo) references Ciclos(id)
+);
+
+create table Tutores(
+	id int auto_increment primary key,
+    fechainicio date,
+    fechafinal date,
+    dni_profesor varchar(25),
+    constraint fk_tutores foreign key(dni_profesor) references Profesores(dni)
 );
